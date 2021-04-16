f6f5c62b86bac953134ba7f1faa1d04b6abaa356
alexesca <alexescamore@gmail.com>
2021-04-16 16:01:37 -0600

    Added celsius to fahr

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * Print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300
  */
 
-main() {
+fahrToCelsius() {
     float fahr, celsius;
     int lower, upper, step;
 
@@ -13,9 +13,32 @@ main() {
     step = 20;
 
     fahr = lower;
+    printf("%s %s\n", "Fahrenheit", "Celsius");
     while(fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
+        printf("%6.0f %9.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
+}
+
+celsiusToFahr() {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    celsius = lower;
+    printf("%s %s\n", "Celsius", "Fahrenheit");
+    while(celsius <= upper) {
+        fahr = ((celsius * 9.0)/5.0) + 32.0;
+        printf("%6.0f %9.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
+
+
+main() {
+    celsiusToFahr();
 }
